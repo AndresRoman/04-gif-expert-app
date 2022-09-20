2311aae4f8534b58cc7236feae69c08c0baa77bc
Andres Roman <andresrombus@outlook.com>
2022-09-20 14:41:42 -0500

    clase 77, comunicacion entre componentes

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
 
     const [inputValue, setInputValue] = useState('One Punch');
 
-    const onInputChange = (target) => {
+    const onInputChange = ({ target }) => {
         setInputValue(target.value);
     };
 
     const onSubmit = (event) => {
         event.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+
+        setCategories(categories => [inputValue, ...categories]);
+        setInputValue('');
+
     };
 
     return (
@@ -22,5 +27,5 @@ export const AddCategory = () => {
             />
         </form>
 
-    )
+    );
 }
