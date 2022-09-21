1f5faa50d7bfba1345a549962f2fb5a95201884b
Andres Roman <andresrombus@outlook.com>
2022-09-21 09:26:23 -0500

    clase 81, Fetch API

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,29 @@
 
 export const GifGrid = ({ category }) => {
 
+    const getGifs = async () => {
 
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=3DLLhl8PPzyBdCB6DlnoYCivjMWgf2yc&q=${category}&limit=20`
+        const resp = await fetch(url);
+        const { data } = await resp.json();
+
+        const gifs = data.map(img => ({
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url,
+        }));
+
+        console.log(gifs);
+
+    }
+
+    getGifs();
 
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola Mundo</p>
         </>
     )
 }
+
+//3DLLhl8PPzyBdCB6DlnoYCivjMWgf2yc
