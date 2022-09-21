ba7826cbbf128ed2f82cecc9a0ed0a8f9e1b5fe1
Andres Roman <andresrombus@outlook.com>
2022-09-21 10:23:15 -0500

    clase 85, className

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
@@ -17,11 +18,16 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{category}</h3>
-            {
-                images.map(({ id, title }) => (
-                    <li key={id}>{title}</li>
-                ))
-            }
+            <div className="card-grid">
+                {
+                    images.map((image) => (
+                        <GifItem
+                            key={image.id}
+                            {...image}
+                        />
+                    ))
+                }
+            </div>
         </>
     )
 }
