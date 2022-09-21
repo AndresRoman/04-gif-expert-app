ef6c6bf76f5289d1827d9a6f95c18a51739db51a
Andres Roman <andresrombus@outlook.com>
2022-09-21 10:11:45 -0500

    clase 84, mostrar titles

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+
     useEffect(() => {
-        getGifs(category);
+        getImages();
     }, []);
 
     return (
         <>
             <h3>{category}</h3>
+            {
+                images.map(({ id, title }) => (
+                    <li key={id}>{title}</li>
+                ))
+            }
         </>
     )
 }
