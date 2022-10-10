5328929ad98a651d0a12d2a5bbd7a32935090388
Andres Roman <andresrombus@outlook.com>
2022-10-10 09:23:07 -0500

    clase 106, jest functions

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -17,8 +17,9 @@ describe('prueba en <AddCategory />', () => {
     test('debe de llamar onNewCategory si el input tiene un valor', () => {
 
         const inputValue = 'Saitama';
+        const onNewCategory = jest.fn();
 
-        render(<AddCategory onNewCategory={() => { }} />);
+        render(<AddCategory onNewCategory={onNewCategory} />);
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
@@ -28,6 +29,20 @@ describe('prueba en <AddCategory />', () => {
 
         expect(input.value).toBe('');
 
+        // expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);
 
+    })
+
+    test('no debe de llamar el onNewCategory si el input esta vacio', () => {
+
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        // expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
     })
 })
