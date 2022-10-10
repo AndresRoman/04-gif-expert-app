5977bc5f98aca8145d25d72d719d19871021942a
Andres Roman <andresrombus@outlook.com>
2022-10-10 09:52:00 -0500

    clase 108, mock de un customHook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from "@testing-library/react"
 import { GifGrid } from "../../src/components/GifGrid"
+import { useFetchGifs } from '../../src/hooks/useFetchGifs'
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('pruebas en <GifGrid />', () => {
 
     const category = 'One Punch';
 
     test('debe de mostrar el loading inicialmente', () => {
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
 
         render(<GifGrid category={category} />);
         // screen.debug();
@@ -16,6 +24,27 @@ describe('pruebas en <GifGrid />', () => {
 
     test('debe de mostrar items cuando se carguen las imagenes useFetchGifs', () => {
 
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg',
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg',
+            },
+        ];
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render(<GifGrid category={category} />);
+
+        expect(screen.getAllByRole('img').length).toBe(2);
 
     })
 })
