a95e06be122d92be8d2664b1cf98baccb74579e0
Andres Roman <andresrombus@outlook.com>
2022-10-10 09:34:08 -0500

    clase 107, pruebas en GifGrid

---
+++ tests/components/GifGrid.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react"
+import { GifGrid } from "../../src/components/GifGrid"
+
+describe('pruebas en <GifGrid />', () => {
+
+    const category = 'One Punch';
+
+    test('debe de mostrar el loading inicialmente', () => {
+
+        render(<GifGrid category={category} />);
+        // screen.debug();
+        expect(screen.getByText('Cargando...'));
+        expect(screen.getByText(category));
+
+    })
+
+    test('debe de mostrar items cuando se carguen las imagenes useFetchGifs', () => {
+
+
+    })
+})
